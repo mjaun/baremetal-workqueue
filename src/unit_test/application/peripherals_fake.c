b66e1ba970765c7a4b8590e3360480d0d58c7d1b
Mario Jaun <mario.jaun@zuehlke.com>
2024-09-16 19:58:09 +0200

    cleanup cmake project

--- src/unit_test/application/peripherals_fake.c
+++ src/unit_test/application/peripherals_fake.c
@@ -1,4 +1,5 @@
 #include "application/peripherals.h"
+#include "util/types.h"
 
 struct gpio_pin
 {
@@ -15,7 +16,7 @@ static struct gpio_pin gpio_user_button;
 
 static struct uart uart_debug;
 
-extern "C" const struct peripherals peripherals = {
+const struct peripherals peripherals = {
     .user_led = &gpio_user_led,
     .user_button = &gpio_user_button,
     .debug_uart = &uart_debug,
